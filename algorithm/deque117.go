a7a7ece05f5c02efa2fbdbd1c2cce3b392ee4311
Today <76273733+today2098@users.noreply.github.com>
2024-08-27 21:19:54 +0900

    [clean] Refactoring a struct, "Deque117"

--- algorithm/deque117.go
+++ algorithm/deque117.go
@@ -14,22 +14,24 @@ type Deque117 struct {
 
 // Create a new deque.
 func NewDeque117() *Deque117 {
-	return &Deque117{Data: list.New()}
+	return &Deque117{
+		Data: list.New(),
+	}
 }
 
 // Checks if the deque is empty.
-func (dq *Deque117) Empty() bool {
-	return dq.Size() == 0
+func (d *Deque117) Empty() bool {
+	return d.Size() == 0
 }
 
 // Returns the number of elements.
-func (dq *Deque117) Size() int {
-	return dq.Data.Len()
+func (d *Deque117) Size() int {
+	return d.Data.Len()
 }
 
 // Returns the first element.
-func (dq *Deque117) Front() interface{} {
-	res := dq.Data.Front()
+func (d *Deque117) Front() interface{} {
+	res := d.Data.Front()
 	if res == nil {
 		panic(ErrDeque117Empty)
 	}
@@ -37,8 +39,8 @@ func (dq *Deque117) Front() interface{} {
 }
 
 // Returns the last element.
-func (dq *Deque117) Back() interface{} {
-	res := dq.Data.Back()
+func (d *Deque117) Back() interface{} {
+	res := d.Data.Back()
 	if res == nil {
 		panic(ErrDeque117Empty)
 	}
@@ -46,43 +48,43 @@ func (dq *Deque117) Back() interface{} {
 }
 
 // Inserts an element at the front.
-func (dq *Deque117) PushFront(x interface{}) {
-	dq.Data.PushFront(x)
+func (d *Deque117) PushFront(x interface{}) {
+	d.Data.PushFront(x)
 }
 
 // Inserts elements at the front.
-func (dq *Deque117) PushFrontRange(v []interface{}) {
+func (d *Deque117) PushFrontRange(v []interface{}) {
 	for i := len(v) - 1; i >= 0; i-- {
-		dq.Data.PushFront(v[i])
+		d.Data.PushFront(v[i])
 	}
 }
 
 // Inserts an element at the back.
-func (dq *Deque117) PushBack(x interface{}) {
-	dq.Data.PushBack(x)
+func (d *Deque117) PushBack(x interface{}) {
+	d.Data.PushBack(x)
 }
 
 // Inserts elements at the back.
-func (dq *Deque117) PushBackRange(v []interface{}) {
+func (d *Deque117) PushBackRange(v []interface{}) {
 	for i := 0; i < len(v); i++ {
-		dq.Data.PushBack(v[i])
+		d.Data.PushBack(v[i])
 	}
 }
 
 // Removes and returns the first element.
-func (dq *Deque117) PopFront() interface{} {
-	res := dq.Data.Front()
+func (d *Deque117) PopFront() interface{} {
+	res := d.Data.Front()
 	if res == nil {
 		panic(ErrDeque117Empty)
 	}
-	return dq.Data.Remove(res)
+	return d.Data.Remove(res)
 }
 
 // Removes and returns the last element.
-func (dq *Deque117) PopBack() interface{} {
-	res := dq.Data.Back()
+func (d *Deque117) PopBack() interface{} {
+	res := d.Data.Back()
 	if res == nil {
 		panic(ErrDeque117Empty)
 	}
-	return dq.Data.Remove(res)
+	return d.Data.Remove(res)
 }
